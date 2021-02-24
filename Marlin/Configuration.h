5035241940dca5184481c29086e786e722019921
aliensr85 <54254054+aliensr85@users.noreply.github.com>
2021-02-24 18:45:54 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -679,7 +679,7 @@
 
   #if ENABLED(ANYCUBIC_KOSSEL_PLUS)
     // Print surface diameter/2 minus unreachable space (avoid collisions with vertical towers).
-    #define DELTA_PRINTABLE_RADIUS 90.0  // (mm)
+    #define DELTA_PRINTABLE_RADIUS 110.0  // (mm)
     // Center-to-center distance of the holes in the diagonal push rods.
     #define DELTA_DIAGONAL_ROD 271.5        // (mm)
       // Horizontal offset from middle of printer to smooth rod center.
